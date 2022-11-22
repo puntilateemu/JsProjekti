b061f4eb4889cc832385e48cb7a7151a1d9f90e0
puntilateemu <puntilateemu@gmail.com>
2022-11-22 10:43:03 +0200

    update

--- src/vahvistus.js
+++ src/vahvistus.js
@@ -1,9 +1,15 @@
 
+
 //haetaan vahvistus numero urlista jos ei numeroa redirect varaukseen
 window.onload = function(){
    var resNum = window.location.href.split('=')[1] //splitataan urli ni saadaan varaus numero
+   var showNum = document.getElementById('1')
    if(resNum == null){
     window.location.href = '/varaus.html' // heitetään takas varaukseen
+   }else{
+      showNum.innerHTML = `Varauksesi nro ${resNum} on vahvistettu`
    }
-   console.log(resNum)
   } 
+
+
+
